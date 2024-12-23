5030d440ca43c5a6f0e057a7ae4ce39f8c641044
awwwyan <109622291+ovenpickled@users.noreply.github.com>
2024-12-23 23:41:13 +0530

    Update Logger

---
+++ demo.py
@@ -0,0 +1,4 @@
+from us_visa.logger import logging
+from us_visa import exception
+
+logging.info("Hello! this is a test")
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="Aryan",
+    author_email="aryanghate29@gmail.com",
+    packages=find_packages()
+)
